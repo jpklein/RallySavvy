b4e8e00c0cb4e6167fffbc616ff5351d68279c11
JP Klein <jp.klein@nbcuni.com>
2014-06-09 17:51:06 -0400

    feat(DefectCtrl): implement basic defect retrieval and detail view

--- src/app/defect/defect-controller.js
+++ src/app/defect/defect-controller.js
@@ -1,25 +1,42 @@
-angular
-  .module('rallySavvy.defect')
-  .controller('DefectCtrl', ['$scope', '$routeParams',
-    function ($scope, $routeParams) {
-      'use strict';
-      $scope.defectId = $routeParams.defectId;
-      // $scope.todos = JSON.parse($window.localStorage.getItem('todos') || '[]');
-      // $scope.$watch('todos', function (newTodos, oldTodos) {
-      //   if (newTodos !== oldTodos) {
-      //     $window.localStorage.setItem('todos', JSON.stringify(angular.copy($scope.todos)));
-      //   }
-      // }, true);
+angular.module('rallySavvy.defect')
+.controller('DefectCtrl', ['$scope', '$routeParams',
+  function ($scope, $routeParams) {
+    'use strict';
+    $scope.defectId = $routeParams.defectId;
 
-      // $scope.add = function () {
-      //   var todo = {label: $scope.label, isDone: false};
-      //   $scope.todos.push(todo);
-      //   $window.localStorage.setItem('todos', JSON.stringify(angular.copy($scope.todos)));
-      //   $scope.label = '';
-      // };
+    var restApi = new RestApi({
+      user: 'username', //required if no api key, defaults to process.env.RALLY_USERNAME
+      pass: 'password', //required if no api key, defaults to process.env.RALLY_PASSWORD
+      // apiKey: '_12fj83fjk...', //preferred, required if no user/pass, defaults to process.env.RALLY_API_KEY
+      apiVersion: 'v2.0', //this is the default and may be omitted
+      server: 'https://rally1.rallydev.com',  //this is the default and may be omitted
+      requestOptions: {
+        headers: {
+          //while optional, it's good practice to provide header information
+          'X-RallyIntegrationName': 'My cool node.js program',
+          'X-RallyIntegrationVendor': 'My company',
+          'X-RallyIntegrationVersion': '1.0'
+        }
+          //any additional request options (proxy options, timeouts, etc.)     
+      }
+    });
 
-      // $scope.check = function () {
-      //   this.todo.isDone = !this.todo.isDone;
-      // };
-    }
-  ]);
+    restApi.get({
+      //may be a ref ('/defect/1234') or an object with a _ref property
+      ref: '/defect/' + $routeParams.defectId, 
+      fetch: ['FormattedID', 'Name'], //fields to fetch
+      // scope: {
+      //     workspace: '/workspace/12345' //optional, only required if creating in non-default workspace
+      // },
+      // requestOptions: {} //optional additional options to pass through to request
+    }).then(function(result) {
+        $scope.$apply(function() {
+          $scope.defectId = "Success!";
+          $scope.defect = result.Object;
+        });
+        console.log(result.Object);
+    }, function(errors) {
+        console.log(errors);
+    });
+  }
+]);
